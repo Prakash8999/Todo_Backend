775378dfbb3b6b593eadb348af9d281489aa11be
Prakash8999 <reshavejha@gmail.com>
2023-05-29 21:41:44 +0530

    Changes1

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ connectDb()
 
 console.log(process.env.PORT);
 app.listen(process.env.PORT, () => {
-	console.log(`Connected to server and port is  ${process.env.PORT} ` );
+	console.log(`Connected to server and port is  ${process.env.PORT} in ${process.env.NODE_ENV}` );
 })
 // now server is our main file
